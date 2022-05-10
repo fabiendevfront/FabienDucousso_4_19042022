2da556b24e507bb9c212443984d27d8fac377285
Fabien Dev <fabiendevfront@gmail.com>
2022-05-10 17:51:37 +0200

    Modal JS refactoring with toggle, start create animation for header and modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -7,55 +7,21 @@
    ====================== */
 
 const modalItem = document.querySelector(".modal");
-const modalBody = document.querySelector(".modal__body");
 const modalContainer = document.querySelector(".modal__container");
-const modalBtn = document.querySelector(".hero__btn");
-const closeBtn = document.querySelector(".modal__close");
-const successBtn = document.querySelector(".modal__btn");
 const modalSuccess = document.querySelector(".modal__success");
 const formItem = document.querySelector(".form-comp");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
 
-/* =============
-   Display modal
-   ============= */
+/* ======================
+   Display and hide modal
+   ====================== */
 
-// Add style display block that display the modal
-const displayModal = () => {
-    modalItem.style.display = "block";
+// Add or remove class active that display the modal
+const toggleModal = () => {
+    modalItem.classList.toggle("active");
     formItem.style.display = "block";
     modalSuccess.style.display = "none";
 }
-
-// Display modal when clicking on button "Je m'inscris" with "displayModal" function
-modalBtn.addEventListener('click', displayModal);
-
-/* ==========
-   Hide modal
-   ========== */
-
-// Add style "display none" that hide the modal
-const hideModal = () => {
-    modalItem.style.display = "none";
-}
-
-// Hide modal when clicking on button "X" with "hideModal" function
-closeBtn.addEventListener('click', hideModal);
-
-
-// Prevents current event propagating up in the DOM
-modalContainer.addEventListener('click', (event) => {
-    event.stopPropagation();
-});
-
-// Hide modal when clicking on modal background with "hideModal" function
-modalItem.addEventListener('click', hideModal);
-
-// Hide modal when clicking on "echap key" with "hideModal" function
-document.addEventListener('keydown', (event) => {
-    if (event.code == 'Escape') {
-        hideModal();
-    }
-});
 
 /* =============
    Success modal
@@ -67,5 +33,21 @@ const displaySuccessModal = () => {
     modalSuccess.style.display = "flex";
 };
 
-// Hide modal when clicking on success modal button "fermer" with "hideModal" function
-successBtn.addEventListener('click', hideModal);
+/* ======
+   Events
+   ====== */
+
+// Assign event function to the buttons. Event execute toggleModal function for display/hide modal.
+modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal));
+
+// Prevents current event propagating up in the DOM
+modalContainer.addEventListener('click', (event) => {
+    event.stopPropagation();
+});
+
+// Hide modal when clicking on "echap key" with "toggleModal" function
+document.addEventListener('keydown', (event) => {
+    if (event.code == 'Escape') {
+        toggleModal();
+    }
+});
